027a3bee497ba33595df749065b2b752e374cc4a
mayism <lican22@huawei.com>
2021-06-17 12:21:03 +0800

    dynamicability/update

--- SplitSampleFeature01/src/main/java/com/huawei/android/dynamicfeaturesplit/splitsamplefeature01/FeatureActivity.java
+++ SplitSampleFeature01/src/main/java/com/huawei/android/dynamicfeaturesplit/splitsamplefeature01/FeatureActivity.java
@@ -1,5 +1,17 @@
 /*
  * Copyright (c) Huawei Technologies Co., Ltd. 2012-2019. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.huawei.android.dynamicfeaturesplit.splitsamplefeature01;
@@ -14,11 +26,9 @@ import android.widget.Toast;
 import com.huawei.hms.feature.dynamicinstall.FeatureCompat;
 
 /**
- *Feature activity.
- *
+ * Feature Activity.
  */
 public class FeatureActivity extends Activity {
-
     private static final String TAG = FeatureActivity.class.getSimpleName();
 
     @Override
@@ -27,7 +37,7 @@ public class FeatureActivity extends Activity {
         try {
             FeatureCompat.install(newBase);
         } catch (Exception e) {
-            Log.w(TAG, "", e);
+            Log.w(TAG, "failed to install", e);
         }
     }
 
@@ -40,8 +50,8 @@ public class FeatureActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feature);
         ImageView mImageView = findViewById(R.id.iv_load_png);
-        mImageView.setImageDrawable(getResources().getDrawable(R.mipmap.google));
-        Toast.makeText(this, "from feature " + stringFromJNI(), Toast.LENGTH_LONG).show();
+        mImageView.setImageDrawable(getResources().getDrawable(R.mipmap.activity));
+        Toast.makeText(this, "from Feature 01: " + stringFromJNI() + " apple is: " + getResources().getString(R.string.apple), Toast.LENGTH_LONG).show();
     }
 
     /**
--- app/src/main/java/com/huawei/android/dynamicfeaturesplit/DynamicFeatureSampleApplication.java
+++ app/src/main/java/com/huawei/android/dynamicfeaturesplit/DynamicFeatureSampleApplication.java
@@ -1,5 +1,17 @@
 /*
  * Copyright (c) Huawei Technologies Co., Ltd. 2012-2019. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.huawei.android.dynamicfeaturesplit;
@@ -11,8 +23,7 @@ import android.util.Log;
 import com.huawei.hms.feature.dynamicinstall.FeatureCompat;
 
 /**
- * Dynamic feature sample application.
- *
+ * Dynamic Feature Sample Application.
  */
 public class DynamicFeatureSampleApplication extends Application {
     /**
@@ -26,7 +37,7 @@ public class DynamicFeatureSampleApplication extends Application {
         try {
             FeatureCompat.install(base);
         } catch (Exception e) {
-            Log.w(TAG, "", e);
+            Log.w(TAG, "install failed", e);
         }
     }
 }
